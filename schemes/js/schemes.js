310a1e54b4e494a7225a7fab6461bdcb9a959073
Siequnu <11821606+Siequnu@users.noreply.github.com>
2021-04-21 15:54:37 +0800

    Add leaflet map to builder

--- schemes/js/schemes.js
+++ schemes/js/schemes.js
@@ -152,6 +152,17 @@ var schemes = (function ($) {
 		// Builder options
 		initBuilder: function ()
 		{
+			// Start Leaflet
+			var mymap = L.map('map').setView([51.505, -0.09], 13);
+			L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+				attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+				maxZoom: 18,
+				id: 'mapbox/streets-v11',
+				tileSize: 512,
+				zoomOffset: -1,
+				accessToken: _settings.mapboxApiKey
+			}).addTo(mymap);
+			
 			// When clicking on the title bar, make it editable
 			$('.builder .map h2').on ('click', function (event){
 				makeContentEditable (event.target);
