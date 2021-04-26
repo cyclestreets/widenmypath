a93a6bbfee65cb53895826d5bcc13007316ba402
mvl22 <martin@lucas-smith.co.uk>
2021-04-26 22:18:56 +0100

    Use tileUrl for map

--- schemes/js/streetvisions.js
+++ schemes/js/streetvisions.js
@@ -8,9 +8,6 @@ var streetvisions = (function ($) {
 		apiBaseUrl: 'https://api.cyclestreets.net',
 		apiKey: 'YOUR_API_KEY',
 		
-		// Mapbox API key
-		mapboxApiKey: 'YOUR_MAPBOX_API_KEY',
-		
 		// Initial map location
 		defaultLatitude: false,
 		defaultLongitude: false,
@@ -387,13 +384,12 @@ var streetvisions = (function ($) {
 		{
 			// Start Leaflet
 			var leafletMap = L.map('map').setView([51.505, -0.09], 13);
-			L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+			L.tileLayer (_settings.tileUrl, {
 				attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
 				maxZoom: 18,
 				id: 'mapbox/streets-v11',
 				tileSize: 512,
-				zoomOffset: -1,
-				accessToken: _settings.mapboxApiKey
+				zoomOffset: -1
 			}).addTo(leafletMap);
 
 			// Allow objects to be draggable onto the map
